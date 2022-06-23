807ceb087f3e8215a27930e14c2354a0145f8052
AbdulrhmanKassemSalla <62542159+AbdulrhmanKassemSalla@users.noreply.github.com>
2022-06-24 00:00:28 +0300

    Added my project

---
+++ unity project/db.php
@@ -0,0 +1,9 @@
+<?php
+    // $con = mysqli_connect("localhost","prime_admission",",iSA%U&s+;N+","prime_cdemo");
+    $con = mysqli_connect("localhost","clotbgve_clothDB","AmEWh7JSHN@a5X8");
+
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
