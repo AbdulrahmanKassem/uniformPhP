fca5ded159490bbb7111208b99c735fe1560aa04
cherqaoui-soft <tdi.abderrahim@gmail.com>
2022-08-29 20:02:33 +0200

    create liste of products

---
+++ products/products_ar.php
@@ -0,0 +1,67 @@
+<?php
+$products_ar[]=new Product('1','كي قطعة قماش عاكس بلون رصاصي التي تثبت على الصدر','مكواة صناعية','images/daleel/image117.jpg','images/daleel/image118.jpg','','','42','EgbtZp_HXww');
+$products_ar[]=new Product('2','خياطة القماش العاكس باللون الرصاصي على صدر افرول العمال','ماكينة حياكة&nbsp; ابرة واحدة','images/daleel/image119.jpg','images/daleel/image120.jpg','SSat','','37','XNPuik1Z03k');
+$products_ar[]=new Product('3','خياطة أوفرلوك لبعض الأجزاء العلوية للأفرول','ماكينة اوفرلوك 3 خيوط','images/daleel/image121.jpg','images/daleel/image122.jpg','EFd','1','41','1cAsP6usE3Y');
+$products_ar[]=new Product('4','تدعيم الجيوب بحبل أو قيطان (Cord)','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image123.jpg','images/daleel/image124.jpg','OSb','2','14','B3GQKaJRjpc');
+$products_ar[]=new Product('5','تثبيت السحابات لجميع الجيوب','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image123.jpg','images/daleel/image125.jpg','SSa','2','21','uznD002oBlo');
+$products_ar[]=new Product('6','وصل الجزء العلوي مع الجزء السفلي للأفرول','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image123.jpg','images/daleel/image126.jpg','SSa','','17','EBPbRd9GBJU');
+$products_ar[]=new Product('7','كي قماش جيوب الصدر','مكواة صناعية','images/daleel/image127.jpg','images/daleel/image128.jpg','','2','33','1RQvsXBG4mM');
+$products_ar[]=new Product('8','خياطة جيب الصدر على الجزء العلوي للأفرهول','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image129.jpg','images/daleel/image130.jpg','SSat','2','3','adHkziQNT28');
+$products_ar[]=new Product('9','وصل الجيب الجانبي','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image131.jpg','images/daleel/image132.jpg','SSa','1','34','qzSgGgbGb8I');
+$products_ar[]=new Product('10','حياكة الجيب الجانبي لأفرول العمال','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image133.jpg','images/daleel/image134.jpg','SSa','1','19','VpTqBvd3MGA');
+$products_ar[]=new Product('11','حياكة الأوفر على أطراف الجيب الجانبي','ماكينة حياكة الأطراف Overedge 3 Needle','images/daleel/image135.jpg','images/daleel/image136.jpg','EFd','','27','CyTkapafmaw');
+$products_ar[]=new Product('12','طريقة تثبيت الفلكرو الخاص بقلاب (رفرف) غطاء الجيب','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image133.jpg','images/daleel/image137.jpg','SSaw','2','15','E3aWAvVQBK0');
+$products_ar[]=new Product('13','تجهيز ( Flaps ) قلاب (رفرف) الجيب القابل للطي','يدويا','images/daleel/image138.jpg','images/daleel/image138.jpg','','','47','Bem9UVzIVRA');
+$products_ar[]=new Product('14','خياطة الجيوب والبطانات والقلابات للجيوب الخلفية للأفرول','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image133.jpg','images/daleel/image139.jpg','SSaw','2','24','3dlOFXjfgEw');
+$products_ar[]=new Product('15','خياطة ( Flaps ) للجيب الخلفي والجيب الكارجو الكبير','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image140.jpg','images/daleel/image141.jpg','SSaw','2','37','wmKXjX2IXKQ');
+$products_ar[]=new Product('16','تجهيز الجزء الأمامي الذي يغطي سحاب الأفرول','يدويا','images/daleel/image142.jpg','images/daleel/image142.jpg','','1','26','LuiPBrs9FNA');
+$products_ar[]=new Product('17','خياطة كسرات الظهر لأفرول العمال','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image143.jpg','images/daleel/image144.jpg','SSa','2','27','BsdAV1CnTyI');
+$products_ar[]=new Product('18','خياطة غطاء السحاب ( Zipper Fly )','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image145.jpg','images/daleel/image146.jpg','SSe','2','10','BsdAV1CnTyI');
+$products_ar[]=new Product('19','خياطة الجزء الخلفي على شكل حرف ( T )','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image147.jpg','images/daleel/image148.jpg','OSa','','21','xFy_NpLTQbg');
+$products_ar[]=new Product('20','خياطة الجزء الخلفي (المقعدة) للأفرول','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image149.jpg','images/daleel/image150.jpg','SSa','','44','V_zgZgr7EZY');
+$products_ar[]=new Product('21','وصل أجزاء الأفرول الخلفية','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image151.jpg','images/daleel/image152.jpg','SSa','','17','5VrumT898pc');
+$products_ar[]=new Product('22','لفق وخياطة أطراف الجيوب','ماكينة حياكة&nbsp; ابرة واحدة Single Needle','images/daleel/image153.jpg','images/daleel/image154.jpg','EFm','1','9','xSOKML98-g8');
+$products_ar[]=new Product('23a','كي الجيوب الخلفية والأجزاء الأمامية للأفرهول','مكواة صناعية','images/daleel/image155.jpg','images/daleel/image156.jpg','','3','47','Ps5SuI5Kryc');
+$products_ar[]=new Product('23b','كي حزام الوسط','مكواة صناعية','images/daleel/image157.jpg','images/daleel/image158.jpg','','','26','Ps5SuI5Kryc');
+$products_ar[]=new Product('24','خياطة الأوفرلوك لقلاب الجيوب','ماكينة حياكة الأطراف Overedge 3 Needle','images/daleel/image159.jpg','images/daleel/image160.jpg','EFd','','29','U-TXQ6ZKPL8');
+$products_ar[]=new Product('25','تثبيت شريط الفلكرو اللاصق لجميع الجيوب العادية والجيب الكارجو (الواسع)؟','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image161.jpg','images/daleel/image162.jpg','SSaw','2','9','TLMYaHE00vo');
+$products_ar[]=new Product('26a','خياطة الجيوب الخلفية والغطاء (القلابات )','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image163.jpg','images/daleel/image164.jpg','SSe','2','38','9recFcq8Pg4');
+$products_ar[]=new Product('26b','خياطة وتثبيت القلاب للجيب الخلفي للأفرهول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image165.jpg','images/daleel/image166.jpg','SSe','','37','aFBn3n4gq8U');
+$products_ar[]=new Product('27','وصل وخياطة الجزء العلوي من الأفرول مع الجزء السفلي','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image165.jpg','images/daleel/image167.jpg','SSa','','34','8MievdgJVt8');
+$products_ar[]=new Product('28a','خياطة وسط الأفرول من الأمام','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image163.jpg','images/daleel/image168.jpg','SSa','','39','u7oeM_rH3sU');
+$products_ar[]=new Product('28b','خياطة وسط الأفرول من الخلف','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image169.jpg','images/daleel/image168.jpg','SSa','','32','ekqepe9C0cA');
+$products_ar[]=new Product('29a','خياطة وتثبيت حزام الوسط مع الأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image165.jpg','images/daleel/image170.jpg','SSab','2','31','S3ciJeTDUkA');
+$products_ar[]=new Product('29b','خياطة حزام الوسط','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image171.jpg','images/daleel/image172.jpg','SSab','1','35','CoypSbTLRF0');
+$products_ar[]=new Product('30','تثبيت وخياطة حبل التقوية أوالقيطان ( Cord )','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image153.jpg','images/daleel/image173.jpg','SSab','3','13','Kpy7oBd3QTc');
+$products_ar[]=new Product('31','تركيب السحاب الأمامي الرئيسي على شريط قماش خارجي','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image169.jpg','images/daleel/image174.jpg','SSab','2','48','kiIadMUpdho');
+$products_ar[]=new Product('32','تثبيت وتركيب شريط السحاب الأمامي على الأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image175.jpg','images/daleel/image170.jpg','SSab','3','38','qMuismkqQbQ');
+$products_ar[]=new Product('33a','خياطة شريط التقوية على الصدر','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image176.jpg','images/daleel/image177.jpg','SSab','2','46','6ai3ti2nwzU');
+$products_ar[]=new Product('33b','خياطة نهاية السحاب الأمامي','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image178.jpg','images/daleel/image179.jpg','SSab','','38','UXCvwRjmbsM');
+$products_ar[]=new Product('34','خياطة الكتف','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image180.jpg','images/daleel/image181.jpg','SSa','2','39','O0DCjbGtQj4');
+$products_ar[]=new Product('35','خياطة شريط الكتافات وتنظيفه وتعديله','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image182.jpg','images/daleel/image183.jpg','SSab','2','37','L0ZG0P9cdtA');
+$products_ar[]=new Product('36a','خياطة الكتافات التي توضع على كتف الأفرهول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image184.jpg','images/daleel/image174.jpg','SSab','1','33','bywzehg7uuA');
+$products_ar[]=new Product('36b','تثبيت شريط الفلكرو اللاصق','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image184.jpg','images/daleel/image185.jpg','SSab','','18','WDLEoMVvn5M');
+$products_ar[]=new Product('37','خياطة الأكتاف وتثبيت شريط الأكتاف','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image182.jpg','images/daleel/image174.jpg','SSab','1','23','6Pd_q23D2W0');
+$products_ar[]=new Product('38','خياطة كم الأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image186.jpg','images/daleel/image187.jpg','SSa','1','8','3yFFe1PpV70');
+$products_ar[]=new Product('39','خياطة فتحة الذراع وتثبيتها مع الأفرهول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image188.jpg','images/daleel/image189.jpg','SSa','2','23','3IefCGieEQg');
+$products_ar[]=new Product('40','غلق وخياطة الجزء الجانبي للأفرهول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image190.jpg','images/daleel/image191.jpg','SSa','2','3','5z3csSKOv_E');
+$products_ar[]=new Product('41a','خياطة الجيوب الكبيرة للأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image188.jpg','images/daleel/image192.jpg','SSa','2','18','xPvyjYWNEJA');
+$products_ar[]=new Product('41b','خياطة الجيوب الكبيرة للأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image193.jpg','images/daleel/image194.jpg','SSa','1','12','VyjZazQv4Co');
+$products_ar[]=new Product('42','كي ياقة الأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image195.jpg','images/daleel/image196.jpg','','','27','HcEtqTMmJe4');
+$products_ar[]=new Product('43a','خياطة الياقة منفصلة','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image197.jpg','images/daleel/image198.jpg','SSa','2','43','MiBufnGXfKo');
+$products_ar[]=new Product('43b','خياطة وصلة تمديد الياقة','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image199.jpg','images/daleel/image200.jpg','SSa','1','14','hmuucIQetVI');
+$products_ar[]=new Product('43c','تثبيت الفلكرو اللاصق لوصلة تمديد الياقة','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image201.jpg','images/daleel/image202.jpg','SSe','','35','P4Qj_ZGJCi8');
+$products_ar[]=new Product('44','وصل الجزء الخاص بتوسيع الياقة','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image203.jpg','images/daleel/image204.jpg','SSa','','11','MeE_7Ah0pJY');
+$products_ar[]=new Product('45','توصيل وخياطة الياقة ووضع تعليمات الغسيل وخياطة الياقة','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image205.jpg','images/daleel/image206.jpg','SSe','3','25','Zq70s8UNeeY');
+$products_ar[]=new Product('46','خياطة الجيب الكبير والقلاب (غطاء الجيب)','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image207.jpg','images/daleel/image208.jpg','SSaw','4','24','nLyaksQx8XA');
+$products_ar[]=new Product('47','تثبيت وخياطة الفلكرو على الكتف','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image207.jpg','images/daleel/image209.jpg','SSat','1','4','MJ-5aIJvKlg');
+$products_ar[]=new Product('48','خياطة طرفي الأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image210.jpg','images/daleel/image211.jpg','SSa','','38','nubxJx-I7jE');
+$products_ar[]=new Product('49','لفق وثني خياطة الأكمام وأرجل الأفرول','ماكينة حياكة  ابرة واحدة Single Needle','images/daleel/image203.jpg','images/daleel/image212.jpg','EFm','3','13','ODsBwu7AKjg');
+$products_ar[]=new Product('50','عمل غ رز التثبيت (الفارماتورة ) للجيوب والسحابات وغطاء الجيوب','ماكينة الفارماتورة','images/daleel/image213.jpg','images/daleel/image214.jpg','Fsf','3','11','joeY6rQZBLk');
+$products_ar[]=new Product('51','تنظيف جميع مناطق الخياطة','مقص يدويا','images/daleel/image215.jpg','images/daleel/image216.jpg','','1','32','tU9z8Pg9VYk');
+
+
+
+
+?>
+
